322cc0cb70a094982eccd846590a5c285ec04b94
Sparkzz <smani2095@gmail.com>
2020-07-02 11:01:24 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -121,7 +121,7 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: Only GOD knows. " + \
+        message_to_reply = f"My Master Has Gone {afk_since}\nWhere He Is: Only GOD knows. " + \
             f"\n\n__ I'll back in a few hours__\n**REASON**: {reason}" \
             if reason \
             else f"**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg) "
